548caa1f21c1d571493131ae41e9e79efacf45c7
Elena <freshjs.ftm@gmail.com>
2022-02-02 10:22:26 +0200

    Refactor loadMore

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -6,8 +6,9 @@ import * as UserActionCreators from '../actions/userCreators';
 const UsersList = (props) => {
   const {users, isFetching, error} = useSelector(({users})=>users);
   const {getUsersRequest} = bindActionCreators(UserActionCreators, useDispatch());
-  const getUsersReq = ({limit, offset}={})=>getUsersRequest({limit, offset});
-  useEffect(() => { getUsersReq() }, []);
+
+  const loadMore = ({limit, offset}={})=>getUsersRequest({offset:users.length});
+  useEffect(() => { loadMore() }, []);
   return (
     <section>
       <h2>Users list</h2>
@@ -16,7 +17,7 @@ const UsersList = (props) => {
       <ul>
         {users.map(u=>(<li key={u.id}>{u.email}</li>))}
       </ul>
-      <button onClick={()=>{getUsersReq({offset:users.length})}}>load more</button>
+      <button onClick={loadMore}>load more</button>
     </section>
   );
 }
